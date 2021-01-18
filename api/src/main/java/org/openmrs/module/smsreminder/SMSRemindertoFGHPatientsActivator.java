064b1f98a8e3bb529f8653dbc6e3d677c6598387
jpboane <jpboane@gmail.com>
2021-01-18 15:26:38 +0200

    Added support to openmrs 2.x

--- api/src/main/java/org/openmrs/module/smsreminder/SMSRemindertoFGHPatientsActivator.java
+++ api/src/main/java/org/openmrs/module/smsreminder/SMSRemindertoFGHPatientsActivator.java
@@ -14,14 +14,14 @@
 package org.openmrs.module.smsreminder;
 
 
-import org.apache.commons.logging.Log; 
+import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.BaseModuleActivator;
 import org.openmrs.module.ModuleActivator;
-
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
  */
-public class SMSRemindertoFGHPatientsActivator implements ModuleActivator {
+public class SMSRemindertoFGHPatientsActivator extends BaseModuleActivator {
 	
 	protected Log log = LogFactory.getLog(getClass());
 		
